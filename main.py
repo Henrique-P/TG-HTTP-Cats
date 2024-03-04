d356329d8e60aa7aa1c6df726766c9db960817a8
Henrique-P <pedrosantana@gmail.com>
2024-03-04 17:47:50 +0000

    Removed fun cat. Renamed variables and funcs

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import logging
 from telegram import InlineQueryResultPhoto, Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, InlineQueryHandler
-from helper import codeIsValid, randomCode, codeOptions
-from telegramToken import telegramToken
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, InlineQueryHandler, CommandHandler
+from helper import codeIsValid, randomCode, codeMatches
+from telegramToken import token
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
-)
-
-async def cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    codeNumber = update.message.text
-    if (codeIsValid(codeNumber)):
-        await update.message.reply_photo(f"https://http.cat/{codeNumber}")
-        print(f"Kitty {codeNumber} sent to @{update.message.from_user.username}")
-    else:
-       await update.message.reply_text(text="This code does not exist. Here's a random one instead:")
-       await update.message.reply_photo(f"https://http.cat/{randomCode()}")
-       print(f"Random kitty sent @{update.message.from_user.username}")
-                
+)               
 
 async def inline_cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query.query
     if not query:
         return
     results = []
-    for option in codeOptions(query):
+    for option in codeMatches(query):
         results.append(
             InlineQueryResultPhoto(
             id = option,
@@ -33,14 +22,9 @@ async def inline_cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
             photo_url = f"https://http.cat/{option}",
             )
         )
-        
     await update.inline_query.answer(results)
-    print(f'Inline kitty called by @{update.inline_query.from_user.username}')
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(telegramToken).build()
-    cat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND) & filters.Regex(r'(^[1-5]\d{2}$)'), cat)
-    inline_cat_handler = InlineQueryHandler(inline_cat, r'([1-5]\d\d?$)')
-    application.add_handler(cat_handler)
-    application.add_handler(inline_cat_handler)
+    application = ApplicationBuilder().token(token).build()
+    application.add_handler(InlineQueryHandler(inline_cat, r'([1-5]\d\d?$)'))
     application.run_polling()
