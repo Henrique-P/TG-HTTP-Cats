045d3c18b2ea3cfb094b2d3e1956a17272b45a17
Henrique-P <pedrosantana@gmail.com>
2024-03-04 18:23:59 +0000

    added handler for start command

--- main.py
+++ main.py
@@ -20,11 +20,16 @@ async def inline_cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
             id = option,
             thumbnail_url = f"https://http.cat/{option}",
             photo_url = f"https://http.cat/{option}",
+            title = option
             )
         )
     await update.inline_query.answer(results)
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hi!\nCurrently there are no funcions for this bot.\nTry calling me inline with a HTTP Code!")
+    
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(token).build()
-    application.add_handler(InlineQueryHandler(inline_cat, r'([1-5]\d\d?$)'))
-    application.run_polling()
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(InlineQueryHandler(inline_cat, r'([1-5]\d\d?$)'))
+    app.add_handler(CommandHandler('start', start))
+    app.run_polling()
